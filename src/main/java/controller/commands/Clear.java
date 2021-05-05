514d3794fd703f292aee00dd70f5f39ef160ef41
Cloaca_b <martusheved@gmail.com>
2021-05-05 13:53:56 +0300

    commands implementation

--- src/main/java/controller/commands/Clear.java
+++ src/main/java/controller/commands/Clear.java
@@ -1,10 +1,14 @@
 package controller.commands;
 
 import controller.CommandExecutionFailed;
+import model.SpaceMarine;
+
+import java.util.LinkedHashMap;
 
 public class Clear implements Command{
     @Override
     public void execute(String[] args) throws CommandExecutionFailed {
-
+        spaceMarineManager.spaceMarineList = new LinkedHashMap<>();
+        SpaceMarine.setCounter(-1);
     }
 }
--- src/main/java/controller/commands/FilterByWeaponType.java
+++ src/main/java/controller/commands/FilterByWeaponType.java
@@ -1,10 +1,25 @@
 package controller.commands;
 
 import controller.CommandExecutionFailed;
+import model.SpaceMarine;
+import model.Weapon;
+
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class FilterByWeaponType implements Command {
     @Override
     public void execute(String[] args) throws CommandExecutionFailed {
-
+        String value = args[0];
+        try {
+            Weapon weapon = Weapon.valueOf(value);
+        } catch (Exception e) {
+            throw new CommandExecutionFailed("No such weapon type. Try again.");
+        }
+        for (Map.Entry<Integer, SpaceMarine> x : spaceMarineManager.spaceMarineList.entrySet()) {
+            if (x.getValue().getWeaponType() == Weapon.valueOf(value)) {
+                System.out.println(x);
+            }
+        }
     }
 }
